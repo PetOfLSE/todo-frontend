dd29a515161357b03cbe8233f800df6f9d82e608
unname <lwer210@naver.com>
2025-01-29 21:23:32 +0900

    feature: router 로그인 여부 검증 로직 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes';
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: routes
 });
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('access');
+  if(to.path !== '/login' && !token){
+    next('/login');
+  }else{
+    next();
+  }
+});
+
+export default router;
