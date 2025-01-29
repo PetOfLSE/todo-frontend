41a8b7ce9caa10303f58eb7b9664ca6e489d864f
unname <lwer210@naver.com>
2025-01-29 23:22:31 +0900

    feature: 할일 API 로직

---
+++ src/api/TodoApi.ts
@@ -0,0 +1,91 @@
+import type { TodoAddData } from '@/type/TodoAddData';
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+const token = localStorage.getItem('access');
+
+export const list = async () => {
+  try{
+    const response = await axios.get(`${API_URL}/todo/list`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if(response.status === 200){
+      return response;
+    }
+
+    return null;
+
+  }catch(error){
+    throw error;
+  }
+}
+
+export const add = async (data: TodoAddData) => {
+
+  if (data.content.trim() === '') {
+    alert('입력란이 비어있습니다.');
+    return;
+  }
+
+  try{
+    const response = await axios.post(`${API_URL}/todo/add`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if(response && response.status === 200){
+      return response;
+    }
+
+    return null;
+
+  }catch(error){
+    throw error;
+  }
+}
+
+export const toggle = async (todoId: number) => {
+  try{
+    const response = await axios.get(`${API_URL}/todo/complete`, {
+      params: {
+        todoId: todoId
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if(response && response.status === 200){
+      return response;
+    }
+
+    return null;
+  }catch(error){
+    throw error;
+  }
+}
+
+export const del = async (todoId: number) => {
+  try{
+    const response = await axios.delete(`${API_URL}/todo/delete`, {
+      params: {
+        todoId: todoId
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if(response && response.status === 200){
+      return response;
+    }
+
+    return null;
+  }catch(error){
+    throw error;
+  }
+}
